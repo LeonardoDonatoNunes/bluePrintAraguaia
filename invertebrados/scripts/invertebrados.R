a2ba1bd4233567350eb9eee0dbcd38b36d14bcc3
LeonardoDonatoNunes <leonardodonatonunes@yahoo.com.br>
2023-04-19 23:03:19 -0300

    Criação dos mapas de ocorrencia das espécies invasoras

--- invertebrados/scripts/invertebrados.R
+++ invertebrados/scripts/invertebrados.R
@@ -68,6 +68,41 @@ for (i in seq_along(arquivos)) {
   num_species <- apply(intersects, 2, function(x) length(dados_sf$species[x]))
   bacias$numEspecies <- num_species
 
+  sf::write_sf(dados_sf, glue::glue('invertebrados/shp/{nome}_ocorrencia_nativas_pontos.shp'))
   sf::write_sf(bacias, glue::glue('invertebrados/shp/{nome}_ocorrencia_nativas.shp'))
+
+}
+
+# Cria camadas das ocorrências das espécies não nativas
+for (i in seq_along(arquivos)) {
+
+  nome <- names(arquivos)[i]
+  dados <- openxlsx::read.xlsx(arquivos[[i]])
+
+  names(dados) <- stringr::str_to_lower(names(dados))
+
+  if (!'origem' %in% names(dados)) {next}
+
+  colunas <- names(dados) %>% table
+  colunas <- names(colunas[colunas > 1])
+  dup_index <- purrr::map_dbl(colunas, ~max(which(names(dados) == .x)))
+  dados <- dados[, -dup_index]
+
+  dados_sf <-
+    dados %>%
+    dplyr::filter(origem == 'invasor') %>%
+    dplyr::mutate(
+      longitude = decimallongitude,
+      latitude = decimallatitude) %>%
+    st_as_sf(coords = c('longitude', 'latitude'), crs = st_crs(bacias))
+
+  if (nrow(dados_sf) == 0) {next}
+
+  intersects <- st_intersects(dados_sf, bacias)
+  num_species <- apply(intersects, 2, function(x) length(dados_sf$species[x]))
+  bacias$numEspecies <- num_species
+
+  sf::write_sf(dados_sf, glue::glue('invertebrados/shp/{nome}_ocorrencia_invasor_pontos.shp'))
+  sf::write_sf(bacias, glue::glue('invertebrados/shp/{nome}_ocorrencia_invasor.shp'))
 
 }
